4e003e26327f3aa6640642a7da0de395d94ac03f
Deepak Bajwa <masoomjaat861@gmail.com>
2025-03-13 13:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ except ValueError:
 
 
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "{filename}\n\nJoin :- @MRxVOLTZ")
 
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
